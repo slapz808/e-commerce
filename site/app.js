982f44f8e045087bd1ed21bce49df7cb063d877a
slapz808 <numpol166@gmail.com>
2020-01-07 16:13:49 +0700

    menu-color-changes

--- site/app.js
+++ site/app.js
@@ -17,13 +17,10 @@ const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
 let cart = [];
 let buttonsDOM = [];
-//syntactical sugar of writing constructor function
 
 // products
 class Products {
   async getProducts() {
-    // always returns promise so we can add .then
-    // we can use await until promised is settled and return result
     try {
       // let result = await fetch("products.json");
       // let data = await result.json();
@@ -247,14 +244,11 @@ document.addEventListener("DOMContentLoaded", () => {
       ui.cartLogic();
     });
 });
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
